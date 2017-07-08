de54245852d0cd1e01e66741ace5281c76120f97
Mann <cmmann@iastate.edu>
2017-07-08 17:42:02 -0500

    Whole bunch of changes for which I was really bad about committing.

--- server.R
+++ server.R
@@ -9,20 +9,26 @@
 
 #Required packages
 library(shiny)
+library(shinyjs)
 library(stringr)
 library(plyr)
 library(biomaRt)
+
 
 #Required files
 source("functions.R")
 
 shinyServer(function(input, output, session) {
+  
+  dF <- reactiveValues(downloadF = FALSE)
+  oligos <<- NULL
   ########################################################
   ####################INPUT VALIDATION####################
   ########################################################
   
         ###########GUIDE RNA##############################
   #Check to ensure that the guide RNA is properly formatted, actually a guide RNA, etc.
+  
   
   validgRNA <- reactive({
     
@@ -145,10 +151,6 @@ shinyServer(function(input, output, session) {
   
   ######################Microhomology######################
   #Test if microhomology is valid, and troubleshoot if not
-  validMHGene <- reactive({
-    
-  })
-  
   validMHCDna <- reactive({
     if((input$crisprSeq != "") && (input$cDNA != "")){
       if(is.null(validCrisprSeq()) && (is.null(validCDNA()))){
@@ -180,7 +182,7 @@ shinyServer(function(input, output, session) {
         
         validate(
           if(revCount == 0 && count == 1){
-            need(cutIndex - input$mh >= 0, 
+            need(cutIndex - as.numeric(input$mh) >= 0, 
                  paste0("Error: Microhomology length is too long for this CRISPR target sequence and cDNA pairing. ",
                         "Choose a smaller homology length. Maximum allowed homology ", 
                         "length for this cDNA/CRISPR pairing is ", 
@@ -192,22 +194,19 @@ shinyServer(function(input, output, session) {
       }
   })
   
-  validMH <- reactive({
-    
-    
-      
-  })
-  
   ######################Gene ID############################
   #Validate GeneID
   validGeneId <- reactive({
     if(input$geneId != ""){
-      
+      if(length(getEnsemblSpecies) > 1){
         validate(
           need(getEnsemblSpecies(input$geneId) != -1, "Error: The gene ID is not recognized as an ENSEMBL gene ID.")
         )
-      
-      
+      } #else {
+        #if(!pingGeneId(input$geneId)){
+        #  need(!pingGeneId(input$geneId) != -1, paste0("Error: ENSEMBL does not have an entry corresponding to your submitted gene ID: ", input$geneId))
+        #}
+      #}
     } 
   })
   
@@ -230,18 +229,11 @@ shinyServer(function(input, output, session) {
     validCDNA()
   })
   
-  #Print out the results of the microhomology validation
-  output$validmh <- renderText({
-    validMH()
-  })
-  
   output$validmhcdna <- renderText({
     validMHCDna()
   })
   
-  output$validmh <- renderText({
-    validMH()
-  })
+
   #Print out the results of gene ID validation
   output$validgeneid <- renderText({
     if(is.null(validGeneId())){
@@ -261,8 +253,7 @@ shinyServer(function(input, output, session) {
     #Check to ensure that all inputs are valid before accepting
     if(is.null(validgRNA()) &&
        is.null(validCrisprSeq()) &&
-       is.null(validCDNA()) &&
-       is.null(validMH())){
+       is.null(validCDNA())){
       
       resetOutputs()
       #Convert inputs to uppercase
@@ -289,49 +280,58 @@ shinyServer(function(input, output, session) {
       }
       
       #Create the oligos
-      oligos <- doCalculations(toupper(uDNA), 
+      oligos <<- doCalculations(toupper(uDNA), 
                                toupper(input$crisprSeq), 
                                guideRNA, 
-                               input$mh,  
+                               as.numeric(input$mh),  
                                input$padding)
-      
+      dF$downloadF <<-TRUE
       printOutputs(oligos)
     } 
   })
+
   
-#Get cDNA/coding sequence from ENSEMBL
-observeEvent(input$geneIdSubmit, {
-  resetOutputs()
-  
-  if(is.null(validgRNA()) &&
-     is.null(validCrisprSeq()) &&
-     is.null(validMH()) &&
-     is.null(validGeneId())){
+  #Get cDNA/coding sequence from ENSEMBL
+  observeEvent(input$geneIdSubmit, {
+    resetOutputs()
     
-    species <- getEnsemblSpecies(toupper(input$geneId))
-    #dset <- if(input$species == 0){
-    #  "hsapiens_gene_ensembl"
-    #} else if(input$species == 1){
-    #  "drerio_gene_ensembl"
-    #} else if(input$species == 2){
-    #  "dmelanogaster_gene_ensembl"
-    #}
+    withProgress({
     
-    if(input$gRNAtype == 1){
-      guideRNA <- ""
-    } else {
-      guideRNA <- toupper(input$gRNA)
+    
+    if(is.null(validgRNA()) &&
+       is.null(validCrisprSeq()) &&
+       is.null(validGeneId())){
+      
+      setProgress(message = "Identifying Species...")
+      species <- getEnsemblSpecies(toupper(input$geneId))
+      
+      if(input$gRNAtype == 1){
+        guideRNA <- ""
+      } else {
+        guideRNA <- toupper(input$gRNA)
+      }
+      
+      setProgress(message = "Querying ENSEMBL database...")
+      
+      
+      oligos <<- getEnsemblSeq(species[1],
+                              input$geneId, 
+                              toupper(input$crisprSeq), 
+                              guideRNA, 
+                              as.numeric(input$mh))
+      
+      
+      
+      setProgress(message = "Done!")
+      
+      dF$downloadF <<-TRUE
+      
+      printOutputs(oligos)
     }
     
-    oligos <- getEnsemblSeq(species[1],
-                            input$geneId, 
-                            toupper(input$crisprSeq), 
-                            guideRNA, 
-                            input$mh)
-    printOutputs(oligos)
-  }
-
-})
+      
+    })
+  })
 
   #Function to print the output
   printOutputs <- function(oligos){
@@ -356,6 +356,33 @@ observeEvent(input$geneIdSubmit, {
     })
   }
   
+  output$downOut <- renderUI({
+    if(dF$downloadF){
+        downloadButton("downOligos", "Download Oligos")
+    } else{
+  ""
+}
+  })
+  
+  
+  output$downOligos <- downloadHandler(
+    filename = function(){
+      paste(gsub("CDT", "", gsub(" ", "_", Sys.time())), "_oligos.txt")},
+    
+    content = function(file){
+      cat(paste0("5' Forward Oligo: ", oligos[1]), file = file, sep = "\n", append = TRUE)
+      cat(paste0("5' Reverse Oligo: ", oligos[2]), file = file, sep = "\n", append = TRUE)
+      cat(paste0("3' Forward Oligo: ", oligos[3]), file = file, sep = "\n", append = TRUE)
+      cat(paste0("3' Reverse Oligo: ", oligos[4]), file = file, sep = "\n", append = TRUE)
+    }
+
+  )
+  
+  #output$downloadFlag <- observeEvent(downloadF, {
+  #  downloadF
+  #})
+  #outputOptions(output, "downloadFlag", suspendWhenHidden = FALSE)
+  
   ########################################################
   #####################SIDEBAR STUFF######################
   ########################################################
@@ -363,12 +390,12 @@ observeEvent(input$geneIdSubmit, {
   #Input an example to demonstrate
   observeEvent(input$example, {
     reset()
+    
     #Reset the inputs that will not be overwritten to their default values
     updateRadioButtons(session, "gRNAtype", selected = 1)
     updateRadioButtons(session, "cDNAtype", selected = 2)
-    updateRadioButtons(session, "paddingSelection", selected = 1)
     updateTextInput(session, "gRNA", value = "")
-    updateSliderInput(session, "mh", value = 24)
+    updateSelectInput(session, "mh", selected = 24)
     updateSelectInput(session, "padding", selected = 0)
     updateTextInput(session, "crisprSeq", value = "GCGCAGCGAGTCAGTGAGCGAGG")
     updateTextInput(session, "cDNA", value = "CTTTCCTGCGTTATCCCCTGATTCTGTGGATAACCGTATTACCGCCTTTGAGTGAGCTGATACCGCTCGCCGCAGCCGAACGACCGAGCGCAGCGAGTCAGTGAGCGAGGAAGCGGAAGAGCGCCCAATACGCAAACCGCCTCTCCCCGCGCGTTGGCCGATTCATTAATGCAGCTGGCACGACAGGTTTCCCGACTGGAAAGCGGGCAGTGAGCGCAACGCAATTAATACGCGTACCGCTAGCCAGGAAGAGTTTGTAGAAACGCAAAAAGGCCATCCGTCAGGATGGCCTTCTGCTTAGTTTGATGCCTGGCAGTTTATGGCGGGCGTCCTGCCCGCCACCCTCCGGGCCGTTGCTTCACAACGTTCAAATCCGCTCCCGGCGGATTTGTCCTACTCAGGAGAGCGTTCACCGACAAACAACAGATAAAACGAAAGGCCCAGTCTTCCGACTGAGCCTTTCGTTTTATTTGATGCCTGGCAGTTCCCTACTCTCGCGTTAACGCTAGCATGGATGTTTTCCCAGTCACGACGTTGTAAAACGACGGCCAGTCTTAAGCTCGGGCCCTGCAGCTCTAGAGCTCGAATTCGGGAGCGCAGAGCTGGAGACAGGCAAACGCCTGTCGGATCCGGAGCCACGAACTTCTCTCTGTTAAAGCAAGCAGGAGACGTGGAAGAAAACCCCGGTCCTATGGTGTCTAAGGGCGAAGAGCTGATTAAGGAGAACATGCACATGAAGCTGTACATGGAGGGCACCGTGAACAACCACCACTTCAAGTGCACATCCGAGGGCGAAGGCAAGCCCTACGAGGGCACCCAGACCATGAGAATCAAGGTGGTCGAGGGCGGCCCTCTCCCCTTCGCCTTCGACATCCTGGCTACCAGCTTCATGTACGGCAGCAGAACCTTCATCAACCACACCCAGGGCATCCCCGACTTCTTTAAGCAGTCCTTCCCTGAGGGCTTCACATGGGAGAGAGTCACCACATACGAAGACGGGGGCGTGCTGACCGCTACCCAGGACACCAGCCTCCAGGACGGCTGCCTCATCTACAACGTCAAGATCAGAGGGGTGAACTTCCCATCCAACGGCCCTGTGATGCAGAAGAAAACACTCGGCTGGGAGGCCAACACCGAGATGCTGTACCCCGCTGACGGCGGCCTGGAAGGCAGAAGCGACATGGCCCTGAAGCTCGTGGGCGGGGGCCACCTGATCTGCAACTTCAAGACCACATACAGATCCAAGAAACCCGCTAAGAACCTCAAGATGCCCGGCGTCTACTATGTGGACCACAGACTGGAAAGAATCAAGGAGGCCGACAAAGAGACCTACGTCGAGCAGCACGAGGTGGCTGTGGCCAGATACTGCGACCTCCCTAGCAAACTGGGGCACAAACTTAATTGAGGCGCGCCTCTAGAACTATAGTGAGTCGTATTACGTAGATCCAGACATGATAAGATACATTGATGAGTTTGGACAAACCACAACTAGAATGCAGTGAAAAAAATGCTTTATTTGTGAAATTTGTGATGCTATTGCTTTATTTGTAACCATTATAAGCTGCAATAAACAAGTTAACAACAACAATTGCATTCATTTTATGTTTCAGGTTCAGGGGGAGGTGTGGGAGGTTTTTTCCAACTTTATTATACAAAGTTGGCATTATAAAAAAGCATTGCTTATCAATTTGTTGCAACGAACAGGTCACTATCAGTCAAAATAAAATCATTATTTGGAGCTCCATGGTAGCGTTAACGCGGCCGCGATATCCCCTATAGTGAGTCGTATTACATGGTCATAGCTGTTTCCTGGCAGCTCTGGCCCGTGTCTCAAAATCTCTGATGTTACATTGCACAAGATAAAAATATATCATCATGAACAATAAAACTGTCTGCTTACATAAACAGTAATACAAGGGGTGTTATGAGCCATATTCAACGGGAAACGTCGAGGCCGCGATTAAATTCCAACATGGATGCTGATTTATATGGGTATAAATGGGCTCGCGATAATGTCGGGCAATCAGGTGCGACAATCTATCGCTTGTATGGGAAGCCCGATGCGCCAGAGTTGTTTCTGAAACATGGCAAAGGTAGCGTTGCCAATGATGTTACAGATGAGATGGTCAGACTAAACTGGCTGACGGAATTTATGCCTCTTCCGACCATCAAGCATTTTATCCGTACTCCTGATGATGCATGGTTACTCACCACTGCGATCCCCGGAAAAACAGCATTCCAGGTATTAGAAGAATATCCTGATTCAGGTGAAAATATTGTTGATGCGCTGGCAGTGTTCCTGCGCCGGTTGCATTCGATTCCTGTTTGTAATTGTCCTTTTAACAGCGATCGCGTATTTCGTCTCGCTCAGGCGCAATCACGAATGAATAACGGTTTGGTTGATGCGAGTGATTTTGATGACGAGCGTAATGGCTGGCCTGTTGAACAAGTCTGGAAAGAAATGCATAAACTTTTGCCATTCTCACCGGATTCAGTCGTCACTCATGGTGATTTCTCACTTGATAACCTTATTTTTGACGAGGGGAAATTAATAGGTTGTATTGATGTTGGACGAGTCGGAATCGCAGACCGATACCAGGATCTTGCCATCCTATGGAACTGCCTCGGTGAGTTTTCTCCTTCATTACAGAAACGGCTTTTTCAAAAATATGGTATTGATAATCCTGATATGAATAAATTGCAGTTTCATTTGATGCTCGATGAGTTTTTCTAATCAGAATTGGTTAATTGGTTGTAACACTGGCAGAGCATTACGCTGACTTGACGGGACGGCGCAAGCTCATGACCAAAATCCCTTAACGTGAGTTACGCGTCGTTCCACTGAGCGTCAGACCCCGTAGAAAAGATCAAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCTTGCAAACAAAAAAACCACCGCTACCAGCGGTGGTTTGTTTGCCGGATCAAGAGCTACCAACTCTTTTTCCGAAGGTAACTGGCTTCAGCAGAGCGCAGATACCAAATACTGTCCTTCTAGTGTAGCCGTAGTTAGGCCACCACTTCAAGAACTCTGTAGCACCGCCTACATACCTCGCTCTGCTAATCCTGTTACCAGTGGCTGCTGCCAGTGGCGATAAGTCGTGTCTTACCGGGTTGGACTCAAGACGATAGTTACCGGATAAGGCGCAGCGGTCGGGCTGAACGGGGGGTTCGTGCACACAGCCCAGCTTGGAGCGAACGACCTACACCGAACTGAGATACCTACAGCGTGAGCATTGAGAAAGCGCCACGCTTCCCGAAGGGAGAAAGGCGGACAGGTATCCGGTAAGCGGCAGGGTCGGAACAGGAGAGCGCACGAGGGAGCTTCCAGGGGGAAACGCCTGGTATCTTTATAGTCCTGTCGGGTTTCGCCACCTCTGACTTGAGCGTCGATTTTTGTGATGCTCGTCAGGGGGGCGGAGCCTATGGAAAAACGCCAGCAACGCGGCCTTTTTACGGTTCCTGGCCTTTTGCTGGCCTTTTGCTCACATGTT")
@@ -384,11 +411,10 @@ observeEvent(input$geneIdSubmit, {
     
     #Reset the inputs to their default values
     updateRadioButtons(session, "gRNAtype", selected = 1)
-    updateSliderInput(session, "mh", value = 24)
+    updateSelectInput(session, "mh", selected = 24)
     
     #Reset the inputs that will not be overwritten to their default values
     updateRadioButtons(session, "cDNAtype", selected = 1)
-    #updateSelectInput(session, "species", selected = 0)
     updateTextInput(session, "geneId", value = "ENSG00000146648")
     updateTextInput(session, "crisprSeq", value = "TGCCACAACCAGTGTGCTGCAGG")
   })
@@ -396,19 +422,24 @@ observeEvent(input$geneIdSubmit, {
   
   #Reset function
   reset <- function(){
+    
     #Reset the inputs to their default values
+    dF$downloadF <<- FALSE
+    oligos <<- NULL
+    updateRadioButtons(session, "plasmidCond", selected = 0)
     updateRadioButtons(session, "gRNAtype", selected = 1)
     updateTextInput(session, "gRNA", value = NA)
     updateTextInput(session, "crisprSeq", value = NA)
     updateRadioButtons(session, "cDNAtype", selected = 2)
     updateTextInput(session, "cDNA", value = NA)
-    updateSliderInput(session, "mh", value = 24)
-    updateRadioButtons(session, "paddingSelection", selected = 1)
+    updateSelectInput(session, "mh", selected = 48)
     updateSelectInput(session, "padding", selected = 0)
+    updateTextInput(session, "geneId", value = NA)
     resetOutputs()
   }
   
   resetOutputs <- function(){
+    
     #Clear the 5' forward oligo output
     output$fivePF <- renderText({
       ""
@@ -426,5 +457,6 @@ observeEvent(input$geneIdSubmit, {
       ""
     })
   }
+  
   
 })
