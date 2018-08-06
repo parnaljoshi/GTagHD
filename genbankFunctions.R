686ea1989dbc2363aae8f07de47b4866343c2f9c
cmmann <cmmann@iastate.edu>
2018-08-06 15:14:54 -0500

    Removed private NCBI API key

--- genbankFunctions.R
+++ genbankFunctions.R
@@ -10,7 +10,7 @@
 
 wonkyGenBankHandler <- function(gba){
   #Fetch the flat file
-  gbFile   <- rentrez:::entrez_fetch(db = "nucleotide", gba, rettype = "gb", api_key = "8f945fc5def00d2dadd40ae4bb1b201bb109")
+  gbFile   <- rentrez:::entrez_fetch(db = "nucleotide", gba, rettype = "gb")
   #Read the flat file line by line
   geneIn   <- unlist(strsplit(gbFile, "\\\n", perl = TRUE))
   #Pass the readLines from flat file to formatApe to create plasmid object
